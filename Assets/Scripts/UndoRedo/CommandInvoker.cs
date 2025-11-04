b2a46d03af64a3389c25fcdb3777b45200631037
김주홍 <gicente1@nate.com>
2025-11-04 17:58:30 +0900

    add undoRedo

---
+++ Assets/Scripts/UndoRedo/CommandInvoker.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections.Generic;
+using System.Windows.Input;
+using UnityEngine;
+
+public interface ICommand
+{
+    void Execute();
+    void Undo();
+}
+
+public class CommandInvoker
+{
+    private Stack<ICommand> undoStack = new Stack<ICommand>();
+    private Stack<ICommand> redoStack = new Stack<ICommand>();
+
+    public void ExecuteCommand(ICommand command) //명령 실행
+    {
+        command.Execute();
+        undoStack.Push(command);
+        redoStack.Clear();
+    }
+
+    public void Undo() //행동 취소 후 이전 상태로 되돌림
+    {
+        if (undoStack.Count > 0)
+        {
+            ICommand command = undoStack.Pop();
+            command.Undo();
+            redoStack.Push(command);
+        }
+    }
+
+    public void Redo() //되돌린 행동 다시 실행
+    {
+        if (redoStack.Count > 0)
+        {
+            ICommand command = redoStack.Pop();
+            command.Execute();
+            undoStack.Push(command);
+        }
+    }
+
+    public bool CanUndo() => undoStack.Count > 0;
+    public bool CanRedo() => redoStack.Count > 0;
+}
+
+public class PushBackCommand : ICommand
+{
+    public List<int> intList;
+    public int numberToAdd;
+
+    public PushBackCommand(List<int> intList, int numberToAdd)
+    {
+        this.intList = intList;
+        this.numberToAdd = numberToAdd;
+    }
+
+    public void Execute()
+    {
+        intList.Add(numberToAdd);
+    }
+
+    public void Undo()
+    {
+        intList.RemoveAt(intList.Count-1);
+    }
+}
+
+public class PopBackCommand : ICommand
+{
+    public List<int> intList;
+    public int indexToRemove;
+    public int number;
+
+    public PopBackCommand(List<int> intList, int indexToRemove)
+    {
+        this.intList = intList;
+        this.indexToRemove = indexToRemove;
+
+        number = intList[indexToRemove];
+    }
+
+    public void Execute()
+    {
+        if (intList.Count > 0)
+            intList.RemoveAt(indexToRemove);
+    }
+
+    public void Undo()
+    {
+        intList.Add(number);
+    }
+}
+
+public class PushFrontCommand : ICommand
+{ 
+    public List<int> intList;
+    public int numberToAdd;
+
+    public PushFrontCommand(List<int> intList, int numberToAdd)
+    {
+        this.intList = intList;
+        this.numberToAdd = numberToAdd;
+    }
+
+    public void Execute()
+    {
+        intList.Insert(0, numberToAdd);
+    }
+
+    public void Undo()
+    {
+        intList.RemoveAt(0);
+    }
+}
+
+public class PopFrontCommand : ICommand
+{
+    public List<int> intList;
+    public int number;
+
+    public PopFrontCommand(List<int> intList)
+    {
+        this.intList = intList;
+        number = intList[0];
+    }
+
+    public void Execute()
+    {
+        if (intList.Count > 0)
+            intList.RemoveAt(0);
+    }
+
+    public void Undo()
+    {
+        intList.Insert(0, number);
+    }
+}
---
+++ Assets/Scripts/UndoRedo/IntListManager.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections.Generic;
+using UnityEditor.Timeline.Actions;
+using UnityEngine;
+
+public class IntListManager : MonoBehaviour
+{
+    //정수 리스트의 **뒤쪽**에서만 요소를 추가(Push)하거나
+    //제거(Pop)할 수 있는 프로그램을 작성하세요.
+
+    //커맨드 패턴을 사용하여 모든 작업에 대해 **Undo(실행 취소)** 와
+    //** Redo(재실행)** 기능을 구현합니다
+
+
+    [Header("테스트 설정")]
+    [SerializeField] private int numberToAdd = 10;
+
+    private List<int> intList = new List<int>();
+    private CommandInvoker invoker = new CommandInvoker();
+
+    private void Start()
+    {
+        Debug.Log("=== 커맨드 패턴 Undo/Redo 연습 시작 ===");
+        PrintList();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.W))
+        {
+            ExecutePushBack();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.S))
+        {
+            ExecutePopBack();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.D))
+        {
+            PrintList();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4) || Input.GetKeyDown(KeyCode.Z))
+        {
+            ExecuteUndo();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha5) || Input.GetKeyDown(KeyCode.X))
+        {
+            ExecuteRedo();
+        }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            ExecutePushFront();
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            ExecutePopFront();
+        }
+    }
+
+    private void ExecutePushBack()
+    {
+            PushBackCommand push = new PushBackCommand(intList, numberToAdd);
+            invoker.ExecuteCommand(push);
+
+            numberToAdd += 10;
+            PrintList();
+    }
+
+    private void ExecutePopBack()
+    {
+        if (intList != null)
+        {
+            int indexToRemove = intList.Count-1;
+            //intList.Remove(numberToRemove);
+            PopBackCommand pop = new PopBackCommand(intList, indexToRemove);
+            invoker.ExecuteCommand(pop);
+            PrintList();
+        }
+    }
+
+    private void ExecutePushFront()
+    {
+        PushFrontCommand pushFront = new PushFrontCommand(intList, numberToAdd);
+        invoker.ExecuteCommand(pushFront);
+
+        numberToAdd += 10;
+        PrintList();
+    }
+
+    private void ExecutePopFront()
+    {
+        if (intList != null)
+        {
+            PopFrontCommand popFront = new PopFrontCommand(intList);
+            invoker.ExecuteCommand(popFront);
+            PrintList();
+        }
+    }
+
+    private void ExecuteUndo()
+    {
+        if (invoker.CanUndo())
+        {
+            invoker.Undo();
+            Debug.Log("<color=cyan>↶ Undo</color>");
+            PrintList();
+        }
+        else
+        {
+            Debug.LogWarning("Undo할 명령이 없습니다!");
+        }
+    }
+
+    private void ExecuteRedo()
+    {
+        if (invoker.CanRedo())
+        {
+            invoker.Redo();
+            Debug.Log("<color=magenta>↷ Redo</color>");
+            PrintList();
+        }
+        else
+        {
+            Debug.LogWarning("Redo할 명령이 없습니다!");
+        }
+    }
+
+    private void PrintList() //근데 그럼 이 리스트가 쓸모가있나..?
+    {
+        Debug.Log($"현재 리스트: [{string.Join(", ", intList)}] (개수: {intList.Count})");
+    }
+}
+
